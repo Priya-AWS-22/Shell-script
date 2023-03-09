a63af439fc346d849245022a8c33906399c2b536
Priya T <priyadally22@gmail.com>
2023-03-10 04:08:09 +0530

    print hello world

---
+++ Shebang.sh
@@ -0,0 +1,12 @@
+#!/bin/bash/
+
+#start of the line any bash script should always be shebang notation
+#Commit is just to add some description
+
+#How to run a script?
+
+#bash scriptname
+#sh scirptname
+#./scriptname
+
+echo "Hello World"
