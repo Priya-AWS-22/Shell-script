559f38ff2933b4f7c231029a9dac0d9610e1a107
Priya T <priyadally22@gmail.com>
2023-03-10 13:12:05 +0530

    variables

---
+++ Variables.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#variable : which holds the value
+
+a=20
+b=50
+c=70
+
+# No datatype in Bash. Everything considered as STRING
+# print $
+# echo $a
+
+echo $a
+echo ${a}
+echo "$a"
+
+echo $d
+
+
+
+
