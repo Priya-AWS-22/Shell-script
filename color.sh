0a4b522e0a61f19bd2109cf9a79c9140e74a868c
Priya T <priyadally22@gmail.com>
2023-03-10 05:51:31 +0530

    color

---
+++ color.sh
@@ -0,0 +1,13 @@
+#!\bin\bash
+
+#https://www.tutorialspoint.com/how-to-output-colored-text-to-a-linux-terminal
+
+#Refer above link for more details
+
+
+#echo -e "\e[COL-CODEm Message \e[0m"
+#echo -e "\e[33m yellow \e[0m"]]"
+
+echo -e "\e[33m yellow \e[0m"
+echo -e "\e[36m cyan \e[0m"
+
