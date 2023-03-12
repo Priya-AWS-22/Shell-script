12378219d927557ec4ec016dcbe5a44c5251dd08
Priya T <priyadally22@gmail.com>
2023-03-12 07:19:47 +0530

    if

--- ifelse.sh
+++ ifelse.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+   echo "simple if condition"
+   
    ACTION=$1
 
-    if [ "$ACTION" == "start" ];  then
+    if [ "$ACTION" == "start" ]; then
        echo -e "\e[32m payment is starting \e[0m"
-
+       exit 0
+    
     else
 
-    if [ "$ACTION" == "stop" ] then 
-       echo -e "\e[31m payment is stopped \e[0m"
+          echo -e "\e[31m payment is stopped \e[0m"
+          exit 3
 
     fi
