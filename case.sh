4a0515b93c26cb7a31c212a33e95367b0e6bc989
Priya T <priyadally22@gmail.com>
2023-03-12 06:15:25 +0530

    case

---
+++ case.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#case $var in
+
+# opt1) cmd1 ;;
+
+# opt2) cmd2 ;;
+
+#esac
+
+case action1 in
+start) 
+echo -e "start the payment"
+;;
+stop)
+echo -e "stop the payment"
+;;
+esac
