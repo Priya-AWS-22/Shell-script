421c43b367d98a9308e236d966b08bb537f310ea
Priya T <priyadally22@gmail.com>
2023-03-17 05:32:07 +0530

    frontend

--- robot/components/frontend.sh
+++ robot/components/frontend.sh
@@ -1,27 +1,57 @@
-#!/bin/bash
+#!/bin/bash 
 
-set -e
+# set -e 
 
+COMPONENT=frontend
+LOGFILE="/tmp/$COMPONENT.log"
+
+# Validting whether the executed user is a root user or not 
 ID=$(id -u)
-if [ "$ID" -ne 0 ] ; then
-   echo -e "\e[31m Excute this script as root or sudo \e[0m"
-   exit 1
-fi
 
-yum install nginx -y
+if [ "$ID" -ne 0 ] ; then 
+    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
+    exit 1
+fi 
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+stat() {
+    if [ $1 -eq 0 ] ; then 
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+        exit 2
+    fi 
+}
 
+echo -n "Installing Ngnix : "
+yum install nginx -y &>> $LOGFILE
+stat $? 
+
+echo -n "Downloading the $COMPONENT component :"
+curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/$COMPONENT/archive/main.zip"
+stat $? 
+
+echo -n "Performing Cleanup of Old $COMPONENT Content :"
 cd /usr/share/nginx/html
-rm -rf *
-unzip /tmp/frontend.zip
-mv frontend-main/* .
-mv static/* .
-rm -rf frontend-main README.md
-mv localhost.conf /etc/nginx/default.d/roboshop.conf
+rm -rf *  &>> $LOGFILE
+stat $? 
 
-systemctl enable nginx
-systemctl start nginx
+echo -n "Copying the downloaded $COMPONENT content: "
+unzip /tmp/$COMPONENT.zip  &>> $LOGFILE
+mv $COMPONENT-main/* .
+mv static/* .
+rm -rf $COMPONENT-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+stat $? 
+
+for component in catalogue cart user shipping payment; do 
+    echo -n "Updating the proxy details in the reverse proxy file :"
+    sed -i "/$component/s/localhost/$component.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done 
+
+echo -n "Starting the service: "
+systemctl enable nginx  &>> $LOGFILE
+systemctl restart nginx &>> $LOGFILE
+stat $? 
 
 
 # 3 observation:
