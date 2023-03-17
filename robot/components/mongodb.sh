4d719d0edcbad7fcf38e42a129f90b99a7729160
Priya T <priyadally22@gmail.com>
2023-03-18 05:03:54 +0530

    mongodb

---
+++ robot/components/mongodb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash 
+
+# set -e 
+
+COMPONENT=mongo
+LOGFILE="/tmp/$COMPONENT.log"
+
+# Validting whether the executed user is a root user or not 
+ID=$(id -u)
+
+if [ "$ID" -ne 0 ] ; then 
+    echo -e "\e[31m You should execute this script as a root user or with a sudo as prefix \e[0m" 
+    exit 1
+fi 
+
+stat() {
+    if [ $1 -eq 0 ] ; then
+        echo -e "\e[32m Success \e[0m"
+    else 
+        echo -e "\e[31m Failure \e[0m"
+        exit 2
+fi 
+
+}
+
+echo -n "configuring the $COMPONENT repo"
+curl -s -o /etc/yum.repos.d/mongodb.repo https://raw.githubusercontent.com/stans-robot-project/mongodb/main/mongo.repo
+stat $?
+
+echo -n "installing $COMPONENT"
+yum install -y mangodb-org &>> $LOGFILE
+stat $?
